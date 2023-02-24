3969e1c13fbdfda6adeec140d643b1563f540cf4
Alisa <alisa.luoma@hotmail.fi>
2023-02-24 15:34:30 +0200

    Button and handling

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,23 @@
-const Counter = () => {
-  return(
-    <div>
-      <p>Counter</p>
-    </div>
-  )
+import React from "react";
+
+export default class Counter extends React.Component {
+
+  handleAttack(){
+    alert("Attack clicked!")
+  }
+
+  handleDefence(){
+    alert("Defent clicked!")
+  }
+
+  render(){
+    return(
+      <div className="row text-white">
+        <h1>Counter:</h1>
+        <button onClick={this.handleAttack} style={{width: "200px"}}>+1</button>
+        <button onClick={this.handleDefence} style={{width: "200px"}}>-1</button>
+      </div>
+    )
+  }
 }
 
-export default Counter;
